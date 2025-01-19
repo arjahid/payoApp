186f4551a7dcdf0e6b447f426ce0ce0aff3c901b
Jahid-Hasan <jhjahidhasanar@gmail.com>
2025-01-19 11:25:18 +0600

    adding money section

--- js/home.js
+++ js/home.js
@@ -2,4 +2,14 @@ document.getElementById('btn-add-money').addEventListener('click',(event)=>{
    event.preventDefault();
    const addMoney=document.getElementById('add-money').value
    const pinNumber=document.getElementById('pin').value;
+   if(pinNumber ==='25800'){
+    const balance=document.getElementById('balance').innerText;
+    const addMoneyNumber=parseInt(addMoney);
+    const balanceNumber=parseInt(balance);
+    const newBalance=addMoneyNumber + balanceNumber;
+    document.getElementById('balance').innerText=newBalance;
+
+   }else{
+    alert("please try again")
+   }
 })
