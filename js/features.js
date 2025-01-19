23c7cf36e05b1d30a19bc4731a694e2847585419
Jahid-Hasan <jhjahidhasanar@gmail.com>
2025-01-19 23:28:14 +0600

    adding functionality

--- js/features.js
+++ js/features.js
@@ -1,8 +1,16 @@
-document.getElementById('btn-show-cashout').addEventListener('click',()=>{
-    document.getElementById('cash-out-form').classList.remove('hidden')
-    document.getElementById("add-money-form").classList.add('hidden')
-})
-document.getElementById('btn-show-addmoney').addEventListener('click',()=>{
-    document.getElementById('cash-out-form').classList.add('hidden')
-    document.getElementById("add-money-form").classList.remove('hidden')
-})
+document.getElementById("btn-show-cashout").addEventListener("click", () => {
+  document.getElementById("cash-out-form").classList.remove("hidden");
+  document.getElementById("add-money-form").classList.add("hidden");
+  document.getElementById("trnsaction-section").classList.add("hidden");
+});
+document.getElementById("btn-show-addmoney").addEventListener("click", () => {
+  document.getElementById("add-money-form").classList.remove("hidden");
+  document.getElementById("cash-out-form").classList.add("hidden");
+
+  document.getElementById("trnsaction-section").classList.add("hidden");
+});
+document.getElementById("transaction-button").addEventListener("click", () => {
+  document.getElementById("trnsaction-section").classList.remove("hidden");
+  document.getElementById("add-money-form").classList.add("hidden");
+  document.getElementById("cash-out-form").classList.add("hidden");
+});
