e6b4807fa2f322d8a935b02c86ab6011afccdeb2
Jahid-Hasan <jhjahidhasanar@gmail.com>
2025-01-20 00:11:01 +0600

    transaction history and validation

--- js/cashout.js
+++ js/cashout.js
@@ -2,12 +2,25 @@ document.getElementById('btn-cash-out').addEventListener('click',(event)=>{
     event.preventDefault();
     const cashOut=document.getElementById('cash-out').value
     const pin=document.getElementById('pin-cash').value
+    if(isNaN(cashOut)){
+        alert("fail to added money")
+        return;
+    }
     if(pin ==='25800'){
         let balance=document.getElementById('balance').innerText
+        if(balance<cashOut){
+            alert("you do not have money")
+            return;
+        }
         let cashInNumber=parseInt(cashOut);
         let balanceInNumber=parseInt(balance);
         let newBalance=balanceInNumber-cashInNumber;
-        document.getElementById('balance').innerText=newBalance
+        document.getElementById('balance').innerText=newBalance;
+        const div=document.createElement('div');
+        div.innerHTML=`
+        <h5>Cash Out</h5>
+        <p>${cashOut} withdraw.New balance ${newBalance}</p>`
+        document.getElementById('transaction-container').appendChild(div)
         
         
     }else{
--- js/home.js
+++ js/home.js
@@ -2,6 +2,10 @@ document.getElementById('btn-add-money').addEventListener('click', (event) => {
     event.preventDefault();
     const addMoney = document.getElementById('add-money').value;
     const pinNumber = document.getElementById('pin').value;
+    if(isNaN(addMoney)){
+        alert("fail to added money")
+        return;
+    }
     if (pinNumber === '25800') {
         const balance = document.getElementById('balance').innerText;
         const addMoneyNumber = parseInt(addMoney);
